de8b0caae94c31a214a51ab56d70ac7107ae355c
sri-13-nidhi <2022cs0317@svce.ac.in>
2024-09-23 01:22:36 +0530

    updated passport.js

--- student-side/config/passport.js
+++ student-side/config/passport.js
@@ -34,7 +34,7 @@ passport.deserializeUser(async function (id, done) {
         const user = await userModel.findById(id);
         
         if (!user) {
-            return done(null, false); // If no user is found, pass null for error and false for user
+            return done(null, false); 
         }
 
         return done(null, user);
