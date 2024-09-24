a0474341b05b7da765938f31ffe6573c86adb875
Vanjinathan <115717134+august-2004@users.noreply.github.com>
2024-09-24 21:40:44 +0530

    scanner can now delete the order from the database

--- admin-side/frontend/src/QrScanner.jsx
+++ admin-side/frontend/src/QrScanner.jsx
@@ -1,32 +1,60 @@
 import { useState,useEffect } from 'react'
 import {Html5QrcodeScanner} from 'html5-qrcode'
 
-function App() {
-  
-
-    const [scanResult, setScanResult] = useState("") 
+function QrScanner() {
+    const [deleteResult,setDeleteResult] = useState("")
   
     useEffect(()=>{
       const scanner = new Html5QrcodeScanner("qrScanner", {
         qrbox:{
           width:300,
           height: 300,
-        }
+        },
+        fps:1
       })
       scanner.render(successFunc,errorFunc);
-      function successFunc(result){
+
+      function successFunc(result){  
+        deleteData(result);
         scanner.clear();
-        setScanResult(result);
+        setTimeout(() => {
+            scanner.render(successFunc,errorFunc);
+        }, 5000);
       } 
       function errorFunc(err){
         console.log(err)
       }
+
+
+      const deleteData = async (id) => {
+        try {
+                console.log(id);
+                const response = await fetch(`http://localhost:3000/orders/${id}`, {
+                method: 'DELETE',
+            });
+    
+            if (response.status === 404) {
+                setDeleteResult(`NOT approved`);
+                throw new Error('Order not found');
+            } 
+            else if (!response.ok) {
+                setDeleteResult(`Error in approving order`);
+                throw new Error(`Unexpected error: ${response.status}`);
+            }
+            else{
+                const result = await response.json();
+                setDeleteResult(`Order is approved`);
+            }
+        } catch (error) {
+          setDeleteResult(`Error in approving order`);
+        }
+      };
+
     },[])
     return (
       <>
-          <div id="qrScanner"></div>
+            <div id="delete result">{deleteResult}</div>
+            <div id="qrScanner"></div>
       </>
     )
-  }
-  
-  export default App
+  }export default QrScanner
