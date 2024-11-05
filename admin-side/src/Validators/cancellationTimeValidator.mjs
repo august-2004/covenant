3491a35fa23307368dacc4a8f62e54cdd3ac9817
Vedhavalli Sarkunnan <vedhavallisarkunnan@gmail.com>
2024-11-05 21:36:19 +0530

    updated cancellation function

--- admin-side/src/Validators/cancellationTimeValidator.mjs
+++ admin-side/src/Validators/cancellationTimeValidator.mjs
@@ -1,3 +1,4 @@
+import { Mealtime } from "../Schemas/MealtimeSchema.mjs";
 import { Order } from "../Schemas/OrderSchema.mjs";
 
 export const cancellationTimeValidator = async (id)=>{
@@ -5,14 +6,15 @@ export const cancellationTimeValidator = async (id)=>{
   if(!orderToCancel){
     return false;
   }
-  const { createdAt } = orderToCancel;
-  const [ orderHours, orderMinutes ] = createdAt.split(":").map(Number);
-  const cancellationTime = ( orderHours*60 ) + orderMinutes + 1;
+  const { mealTime } = orderToCancel;
+  const {closingTime} = await Mealtime.findOne({mealtime:mealTime});
+  const [closingTimeHours, closingTimeMinutes] = closingTime.split(":").map(Number);
+  const cancellationTime = ( closingTimeHours*60 ) + closingTimeMinutes -30;
   const now = new Date();
   const currentTime = ( now.getHours()*60 ) + now.getMinutes();
   if(currentTime<cancellationTime){
-    console.log(currentTime-cancellationTime);
     return true;
   }
+  console.log("Time limit exceeded");
   return false;
 }
